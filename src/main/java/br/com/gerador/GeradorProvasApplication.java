f37d74244cfe2c2a78c0ff31e4331c50a22d870b
Matheus Inácio Gerhardt <matheusgerhardt96@gmail.com>
2022-03-01 20:54:48 -0300

    1st commit

---
+++ src/main/java/br/com/gerador/GeradorProvasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeradorProvasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeradorProvasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gerador/GeradorProvasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeradorProvasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
